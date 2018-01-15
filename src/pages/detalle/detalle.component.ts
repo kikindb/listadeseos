2fe522449dff02227b00416c45d09d261790a243
Jesus Enrique Dick Bustamante <enriquedb2@gmail.com>
2018-01-15 00:26:15 -0600

    Alerta de confirmacion implementada al momento de querer borrar una lista

--- src/pages/detalle/detalle.component.ts
+++ src/pages/detalle/detalle.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { Listas } from '../../app/classes/listas';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { Listas, ListaItem } from '../../app/classes/index';
+import { ListaDeseosService } from '../../app/services/listadeseos.service';
+
 
 @Component({
     selector: 'app-detalle',
@@ -12,11 +14,43 @@ export class DetalleComponent implements OnInit {
     lista: Listas;
 
     constructor(public navCtrl: NavController,
-                public navParams: NavParams
+                public navParams: NavParams,
+                public _listaDeseos: ListaDeseosService,
+                public alertCtrl: AlertController
     ){
         this.idx = this.navParams.get('idx');
         this.lista = this.navParams.get('lista');
      }
 
     ngOnInit() { }
+
+    actualizar(item: ListaItem){
+        item.isComplete = !item.isComplete;
+        this._listaDeseos.actualizarData();
+    }
+
+    borrarLista(){
+        this.showConfirm();
+    }
+    showConfirm() {
+        let confirm = this.alertCtrl.create({
+          title: 'Desea eliminar la lista?',
+          message: 'Esta seguro que desea eliminar esta lista?',
+          buttons: [
+            {
+              text: 'Cancelar',
+              handler: () => {
+                console.log('Disagree clicked');
+              }
+            },
+            {
+              text: 'Aceptar',
+              handler: () => {
+                console.log('Agree clicked');
+              }
+            }
+          ]
+        });
+        confirm.present();
+    }
 }
